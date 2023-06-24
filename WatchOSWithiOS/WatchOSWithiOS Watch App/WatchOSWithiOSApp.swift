92ebe1ef4de5a76be09205c73e50be8de48761f9
Moonkey <moonkey48@icloud.com>
2023-06-24 09:50:53 +0900

    [Setting] WatchOSWithiOS: start project

---
+++ WatchOSWithiOS/WatchOSWithiOS Watch App/WatchOSWithiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSWithiOSApp.swift
+//  WatchOSWithiOS Watch App
+//
+//  Created by Seungui Moon on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchOSWithiOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchOSWithiOS/WatchOSWithiOS/WatchOSWithiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSWithiOSApp.swift
+//  WatchOSWithiOS
+//
+//  Created by Seungui Moon on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchOSWithiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
